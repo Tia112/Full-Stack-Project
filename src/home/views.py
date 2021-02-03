78c6fe412fd9019d5f5ca050421e0d542fecb857
Tia112 <48679960+Tia112@users.noreply.github.com>
2021-02-03 12:38:29 +0000

    Building Model, Views, Templates
    - Created models, views, templates for category choice in menu

--- src/home/views.py
+++ src/home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request)
+
+    products
+    context = {}
+    return render(request, "home.html", context=context)
+
--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,44 @@
+# Import Django libraries
+
 from django.db import models
+from django.shortcuts import reverse
+
+
+# Create Categroy Choices for Menu
+
+CATEGORY_CHOICES = (
+    ('SND', 'SUNDAES'),
+    ('WFF', 'WAFFLES'),
+    ('CRP', 'CREPES'),
+    ('COK', 'COOKIES'),
+    ('CKS', 'CAKES'),
+    ('MLK', 'MILKSHAKES')
+)
 
 # Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=3)
+    description = models.TextField()
+    image = models.ImageField()
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("core:product", kwargs={
+            'slug': self.slug
+        })
+
+    def get_add_to_cart_url(self):
+        return reverse("core:add-to-cart", kwargs={
+            'slug': self.slug
+        })
+
+    def get_remove_from_cart_url(self):
+        return reverse("core:remove-from-cart", kwargs={
+            'slug': self.slug
+        })
