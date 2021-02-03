68f29ca794556ea913a5f4f9c716c7f6572aa555
Tia112 <48679960+Tia112@users.noreply.github.com>
2021-02-03 13:32:37 +0000

    Added base.html
    Building out html files for user site
    - base/navbar
    - navbar uses Bootstrap functionality

--- src/home/views.py
+++ src/home/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from products.models import Item
 
 # Create your views here.
 
 def index(request)
 
-    products
-    context = {}
+    products = Item.objects.all()
+    context = {
+        'products': products
+    }
     return render(request, "home.html", context=context)
 
