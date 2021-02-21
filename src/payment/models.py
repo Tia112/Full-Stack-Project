ccc32748d1720801de39865fbe9680b3b8b80b5b
Tia112 <48679960+Tia112@users.noreply.github.com>
2021-02-21 12:53:29 +0000

    Added Payment model, error control for delivery address, payment, order confirmation

--- src/payment/models.py
+++ src/payment/models.py
@@ -1,2 +1,14 @@
+from django.db import models
+from django.conf import settings
+User_Model = settings.AUTH_USER_MODEL
 
 
+class Payment(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    stripe_bill_id = models.CharField(max_length=50)
+    user = models.ForeignKey(
+        User_Model, on_delete=models.SET_NULL, blank=True, null=True)
+    amt = models.FloatField(default=0.00, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
--- src/payment/views.py
+++ src/payment/views.py
@@ -27,3 +27,71 @@ class Checkout(View):
                 'form': form,
                 'order': order,
             }
+
+
+delivery_address_queryset = Address.objects.filter(
+    user=self.request.user, default=True)
+            if delivery_address_queryset.exists():
+                context.update(
+                    {'default_delivery_address': delivery_address_queryset[0]})
+            return render(self.request, "checkout.html", context)
+        except ObjectDoesNotExist:
+            messages.info(self.request, "You don't have an Active Order")
+            return redirect("checkout")
+
+    def post(self, *args, **kwargs):
+        print(self.request.POST)
+        form = CheckoutForm(self.request.POST or None)
+        try:
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            if form.is_valid():
+                data = form.cleaned_data
+                use_default_delievery = data.get('use_default_delievery')
+                if use_default_delievery:
+                    address_queryset = Address.objects.filter(user=self.request.user,default=True)
+                    if address_queryset.exists():
+                        delivery_address = address_queryset[0]
+                        order.delivery_address = delivery_address
+                        order.save()
+                    else:
+                        messages.info(
+                            self.request, "No Shipping Address Available")
+                        return redirect('checkout')
+                else:
+                    street_address1 = data.get('street_address1')
+                    street_address2 = data.get('street_address2')
+                    shipping_country = data.get('shipping_country')
+                    postal_code = data.get('postal_code')
+
+                    if is_form_valid([street_address1, shipping_country, postal_code]):
+                        delivery_address = Address(
+                            user=self.request.user,
+                            street_address=street_address1,
+                            apartment_address=street_address2,
+                            country=shipping_country,
+                            postal_code=postal_code
+                        )
+                        delivery_address.save()
+
+                        order.delivery_address = delivery_address
+                        order.save()
+
+                        set_default_shipping = data.get('set_default_shipping')
+                        if set_default_shipping:
+                            delivery_address.default = True
+                            delivery_address.save()
+
+                    else:
+                        messages.info(
+                            self.request, "Please fill in the required Shipping Address Fields")
+
+                payment_option = data.get('payment_option')
+
+                if payment_option == 'S':
+                    return redirect('payment', payment_option='stripe')
+                else:
+                    messages.warning(self.request, "Invalid payment option selected!!")
+                    return redirect('checkout')
+        except ObjectDoesNotExist:
+            messages.warning(self.request, "You don't have an active order")
+            return redirect("order-summary")
