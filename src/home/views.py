f392883ad7201ec68f54a515b283f06ec6cd1bda
Tia112 <48679960+Tia112@users.noreply.github.com>
2021-02-28 15:05:39 +0000

    Added order_history html, amended views, URL for removal of single product from cart

--- src/home/views.py
+++ src/home/views.py
@@ -5,22 +5,18 @@ from products.models import Product
 
 
 def index(request):
-    return render(request, "home.html")
+    return render(request, "home.html", {'flag': False})
 
 
 def search(request):
     search_text = request.POST.get('search')
     products = Product.objects.filter(title__icontains=search_text)
-    context = {
-        'products': products
-    }
-    return render(request, "home.html", context=context)
+
+    flag = True
+    return render(request, "home.html", {'products': products, 'flag': flag})
 
 
 def category(request, category):
     products = Product.objects.filter(category=category)
-    context = {
-        'category': category,
-        'products': products
-    }
-    return render(request, "home.html", context=context)
+    flag = True
+    return render(request, "home.html", {'products': products, 'flag': flag, 'category': category})
