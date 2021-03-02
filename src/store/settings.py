e9177026fef7959eeecf68c3edad93577b2c6554
Tia112 <48679960+Tia112@users.noreply.github.com>
2021-03-02 22:19:49 +0000

    Updated static files

--- src/store/settings.py
+++ src/store/settings.py
@@ -13,13 +13,13 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """ Email Settings Test """
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'confirmation.dessertscapital@gmail.com'
-EMAIL_HOST_PASSWORD = 'fuzowmpdjealixqy'
-DEFAULT_FROM_EMAIL = 'Desserts Capital Confirmation<order-confirmation.dessertscapital@gmail.com>'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "confirmation.dessertscapital@gmail.com"
+EMAIL_HOST_PASSWORD = "fuzowmpdjealixqy"
+DEFAULT_FROM_EMAIL = "Desserts Capital Confirmation<order-confirmation.dessertscapital@gmail.com>"
 EMAIL_PORT = 587
 
-BASE_URL = '127.0.0.1:8000'
+BASE_URL = "127.0.0.1:8000"
 
 
 import os
@@ -32,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')#i0kptr#l2rdn0&vm*v!+u6uxizn_c#yh5m1@mzs9s(+&90b#'
+SECRET_KEY = ")#i0kptr#l2rdn0&vm*v!+u6uxizn_c#yh5m1@mzs9s(+&90b#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -43,68 +43,68 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 
     #user defined apps
-    'accounts',
-    'cart',
-    'home',
-    'order',
-    'payment',
-    'products',
+    "accounts",
+    "cart",
+    "home",
+    "order",
+    "payment",
+    "products",
 
     #pre-built apps
-    'django.contrib.sites',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'crispy_forms',
-    'django_countries',
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "crispy_forms",
+    "django_countries",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'store.urls'
+ROOT_URLCONF = "store.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'store.wsgi.application'
+WSGI_APPLICATION = "store.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -114,16 +114,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -131,9 +131,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -145,22 +145,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_store')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_store')
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
+STATIC_ROOT = os.path.join(BASE_DIR, "static_store")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media_store")
 
 
 AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend'
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend"
 )
 SITE_ID = 1
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = "/"
 
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 STRIPE_PUBLIC_KEY = "pk_test_51IOKhCAcNcAz8b39e5pnRwFt6SrgEIFf2twYre4jTuzTqyT9hbczpcDkFvXqx0ssb1L7tgeoPAhSNL5PsAc7POWZ00XQFMiJHx"
 STRIPE_SECRET_KEY = "sk_test_51IOKhCAcNcAz8b39Y9VzO4sT8o7XREtLym8bGaYwfdScYsErLVCJoLi0ZMZ1bAQ9X4xMe4jXTAFjQzcgmf8olHqI00sKxhSXhO"
--- src/store/urls.py
+++ src/store/urls.py
@@ -9,20 +9,20 @@ from products.views import ProductDetailView
 from cart.views import add_to_cart,remove_from_cart,remove_single_item_from_cart
 
 urlpatterns = [
-    path('',index,name='index'),
-    path('admin/',admin.site.urls),
-    path('accounts/',include('allauth.urls')),
-    path('search/',search,name='search'),
-    path('category/<category>',category,name='category'),
-    path('checkout/', Checkout.as_view(),name='checkout'),
-    path('add-to-cart/<slug>/',add_to_cart,name='add-to-cart'),
-    path('remove-from-cart/<slug>/',remove_from_cart,name='remove-from-cart'),
-    path('remove-item-from-cart/<slug>/',remove_single_item_from_cart,name='remove-single-item-from-cart'),
-    path('order-summary/',OrderSummaryView.as_view(),name='order-summary'),
-    path('order-history/', OrderHistory, name='order_history'),
-    path('order-confirmation/<orderid>', OrderConfirm, name='order_confirmation'),
-    path('product/<slug>/',ProductDetailView.as_view(),name='product'),
-    path('payment/<payment_option>/',PaymentPage.as_view(),name='payment'),
+    path("",index,name="index"),
+    path("admin/",admin.site.urls),
+    path("accounts/",include("allauth.urls")),
+    path("search/",search,name="search"),
+    path("category/<category>",category,name="category"),
+    path("checkout/", Checkout.as_view(),name="checkout"),
+    path("add-to-cart/<slug>/",add_to_cart,name="add-to-cart"),
+    path("remove-from-cart/<slug>/",remove_from_cart,name="remove-from-cart"),
+    path("remove-item-from-cart/<slug>/",remove_single_item_from_cart,name="remove-single-item-from-cart"),
+    path("order-summary/",OrderSummaryView.as_view(),name="order-summary"),
+    path("order-history/", OrderHistory, name="order_history"),
+    path("order-confirmation/<orderid>", OrderConfirm, name="order_confirmation"),
+    path("product/<slug>/",ProductDetailView.as_view(),name="product"),
+    path("payment/<payment_option>/",PaymentPage.as_view(),name="payment"),
 ]
 
 if settings.DEBUG:
