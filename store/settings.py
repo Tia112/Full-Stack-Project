cde5f89d9f766f7275efd236e16320603ab83bd8
Tia112 <48679960+Tia112@users.noreply.github.com>
2021-03-07 00:22:00 +0000

    Remove import library

--- store/settings.py
+++ store/settings.py
@@ -24,7 +24,7 @@ BASE_URL = "dessertscapital.herokuapp.com"
 
 import os
 import dj_database_url
-import django heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
