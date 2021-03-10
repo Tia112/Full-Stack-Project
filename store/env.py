421d8e0a9f7107daf453b3c06538907a5048b5d3
Attiya Riaz <48679960+Tia112@users.noreply.github.com>
2021-03-10 12:02:08 +0000

    Delete env.py

--- store/env.py
+++
@@ -1,3 +0,0 @@
-import os
-os.environ.setdefault("SECRET_KEY", "s=+%wf-vdo@0iougy+4=!*q9a-@&(zoqvnn=3y2yi9$+bn=4l$")
-os.environ.setdefault("DATABASE_URL", "postgres://kjdrfrhoygeaev:6a70e1976d6c492af4596761cfd24adca35d2a92a05d034972e1888bc9979e6d@ec2-52-19-96-181.eu-west-1.compute.amazonaws.com: 5432/d2p7a343eleph2")
