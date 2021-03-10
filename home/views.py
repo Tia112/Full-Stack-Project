1aa16d569a703ea6ec81b522a25dd77d74145175
Attiya Riaz <48679960+Tia112@users.noreply.github.com>
2021-03-10 11:52:49 +0000

    Add files via upload

--- home/views.py
+++ home/views.py
@@ -5,10 +5,20 @@ from products.models import Product
 
 
 def index(request):
+    """
+    Render Home Page of the Website
+    :param request: GET
+    :return: Renders home page
+    """
     return render(request, "home.html", {"flag": False})
 
 
 def search(request):
+    """
+    Renders products which contain search_text in their name or description
+    :param request: POST
+    :return: Products based on the search text given
+    """
     search_text = request.POST.get("search")
     products = Product.objects.filter(title__icontains=search_text)
 
@@ -17,6 +27,13 @@ def search(request):
 
 
 def category(request, category):
+    """
+    Renders products which are a part of given category
+    :param request: GET
+    :param category: Product Category Type
+    :return: Products based on the category given
+    """
     products = Product.objects.filter(category=category)
     flag = True
-    return render(request, "home.html", {"products": products, "flag": flag, "category": category})
+    return render(request, "home.html", {"products": products, "flag": flag,
+                                         "category": category})
